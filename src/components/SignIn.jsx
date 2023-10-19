33c63032d5fc7fa0178c4f41dd0262a3b32e1942
RiteshDev123 <riteshchoudharydevsind@gmail.com>
2023-10-19 11:59:40 +0530

    removed unused css code and changes signinpage

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,45 +1,59 @@
 import React, { useState } from 'react';
 
-function SignIn({handleChange, handleSubmit, formData, errors, showAlert}) {
+function SignIn({ handleChange, handleSubmit, formData, errors, showAlert }) {
   return (
-    <div className="container">
+    <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <h2>Sign In</h2>
-          <form onSubmit={handleSubmit}>
-            {showAlert && <p className='text-danger'>Invalid Id or password</p>}
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="email"
-                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-              {errors.email && (
-                <div className="invalid-feedback">{errors.email}</div>
-              )}
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title text-center">Sign In</h2>
+              <form onSubmit={handleSubmit}>
+                {showAlert && (
+                  <div className="alert alert-danger" role="alert">
+                    Invalid email or password
+                  </div>
+                )}
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
+                </div>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">
+                    Sign In
+                  </button>
+                </div>
+              </form>
             </div>
-
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-              {errors.password && (
-                <div className="invalid-feedback">{errors.password}</div>
-              )}
-            </div>
-
-            <button type="submit" className="btn btn-primary">
-              Sign In
-            </button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
